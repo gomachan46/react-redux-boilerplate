d5da956ae5ca42bc81cbdfcae8df37b5630a0ab4
gomachan46 <shiro.gomachan46@gmail.com>
2015-11-23 22:59:02 +0900

    initial commit

---
+++ containers/App.js
@@ -0,0 +1,38 @@
+import React, { Component, PropTypes } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import Counter from '../components/Counter'
+import * as CounterActions from '../actions/counter'
+
+class App extends Component {
+  render() {
+    const { counter, actions } = this.props
+    return (
+      <div>
+        <Counter counter={counter} {...actions} />
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  counter: PropTypes.number.isRequired,
+  actions: PropTypes.object.isRequired
+}
+
+function mapStateToProps(state) {
+  return {
+    counter: state.counter
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(CounterActions, dispatch)
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
